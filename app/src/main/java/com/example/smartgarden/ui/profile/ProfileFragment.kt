982fb6ca4a3450b7a6da624fc05448d9ff3fd20c
MuhRijalulAwal <rijalul9523@gmail.com>
2023-12-22 07:46:29 +0700

    push profile -change some view

--- app/src/main/java/com/example/smartgarden/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/smartgarden/ui/profile/ProfileFragment.kt
@@ -7,15 +7,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.smartgarden.R
-import com.example.smartgarden.databinding.FragmentMyplantBinding
 import com.example.smartgarden.databinding.FragmentProfileBinding
+import com.example.smartgarden.dummy.user
 import com.example.smartgarden.ui.welcome.WelcomeActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
@@ -71,58 +72,57 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentProfileBinding.inflate(layoutInflater)
-        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        val view = binding.root
 
-        // Find your views and set data
-        val tvUsernameProfile = view.findViewById<TextView>(R.id.tv_username_profile)
-        val tvEmailProfile = view.findViewById<TextView>(R.id.tv_email_profile)
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
 
+        // Ambil UID pengguna saat ini
+        if (userId != null) {
+            val userReference = FirebaseDatabase.getInstance().reference.child("users").child(userId)
+            userReference?.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    // Parsing data pengguna menjadi objek UserModel
+                    val userName = snapshot.child("name").getValue(String::class.java)
+                    val userAddress = snapshot.child("address").getValue(String::class.java)
 
+                    // Tampilkan data pengguna ke XML menggunakan data binding
+                    binding.apply {
+                        // Pastikan user tidak null sebelum mengakses propertinya
+                        userName?.let {
+                            // Setel teks nama dan alamat ke TextView di XML
+                            tvUsernameProfile.text = it
+                        }
+                        userAddress?.let {
+                            // Setel teks alamat ke TextView di XML
+                            tvEmailProfile.text = it
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.e("ProfileFragment", "Gagal membaca data pengguna", error.toException())
+                }
+            })
+        } else {
+            Log.e("ProfileFragment", "userId kosong")
+        }
 
         binding.btnLogOut.setOnClickListener {
             // Panggil fungsi logout
             onLogoutClick(view)
         }
-
-        val args = arguments
-        if (args != null) {
-            val username = args.getString("username", "")
-            val address = args.getString("address", "")
-
-            tvUsernameProfile.text = username
-            tvEmailProfile.text = address
-        }
-
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentProfileBinding.inflate(layoutInflater)
-
-        val userId = FirebaseAuth.getInstance().currentUser?.uid
-
-
-       if (userId != null) {
-           val userReference = FirebaseDatabase.getInstance().reference.child("users").child(userId)
-           userReference.addValueEventListener(object : ValueEventListener{
-               override fun onDataChange(snapshot: DataSnapshot) {
-                   val userName = snapshot.child("name").getValue(String::class.java)
-                   val userAddress = snapshot.child("address").getValue(String::class.java)
-                   binding.tvUsernameProfile.text = userName
-                   binding.tvEmailProfile.text = userAddress
-               }
-
-               override fun onCancelled(error: DatabaseError) {
-                   //digunakan untuk handle database error
-                   Log.e("ProfileFragment", "Gagal membaca data pengguna", error.toException())
-               }
-           })
-
-       }
     }
 
+    fun onLogoutClick(view: View) {
+        // Panggil metode logout() atau kode logout yang Anda inginkan di sini
+        logout()
+    }
 
     fun logout() {
         // Hapus sesi login
@@ -132,9 +132,6 @@ class ProfileFragment : Fragment() {
         val intent = Intent(requireContext(),WelcomeActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
         startActivity(intent)
-
-
-//        requireContext().finish()
     }
 
     private fun clearLoginSession() {
@@ -143,13 +140,4 @@ class ProfileFragment : Fragment() {
         editor.clear()
         editor.apply()
     }
-
-    fun onLogoutClick(view: View) {
-        // Panggil metode logout() atau kode logout yang Anda inginkan di sini
-        logout()
-    }
-
-//    override fun onClick(v: View) {
-//
-//    }
 }
