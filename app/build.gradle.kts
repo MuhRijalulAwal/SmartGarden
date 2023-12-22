a51f3266bad2bbc92c4dfb6d881f324c5ec60af7
MuhRijalulAwal <rijalul9523@gmail.com>
2023-12-22 07:44:31 +0700

    push camera and upload feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,9 +50,6 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
-    implementation("com.google.firebase:firebase-auth:22.3.0")
-    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
-    implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("org.tensorflow:tensorflow-lite-support:0.1.0")
     implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0")
     testImplementation("junit:junit:4.13.2")
@@ -65,29 +62,30 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.8.0")
 
     implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    // Retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation ("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
     implementation ("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
 
-    implementation ("androidx.datastore:datastore-preferences:1.0.0")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-
+    // 
     implementation ("androidx.appcompat:appcompat:1.3.0-beta01")
     implementation ("androidx.cardview:cardview:1.0.0")
 
     //Mempercepat inferensi model
     implementation ("org.tensorflow:tensorflow-lite-gpu:2.3.0")
 
-    // Import the Firebase BoM
+    // Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
-
-    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
+    implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-database-ktx")
 
+    // Camera
     val camerax_version = "1.1.0-beta02"
     implementation("androidx.camera:camera-camera2:$camerax_version")
     implementation("androidx.camera:camera-lifecycle:$camerax_version")
--- app/src/main/java/com/example/smartgarden/MainActivity.kt
+++ app/src/main/java/com/example/smartgarden/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.smartgarden
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.cardview.widget.CardView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.smartgarden.databinding.ActivityMainBinding
 import com.example.smartgarden.ui.profile.ProfileFragment
+import com.example.smartgarden.ui.upload.UploadActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +23,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setBottomNavigationView()
+
+        // Find the CardView with ID card_camera
+        val cardCamera: CardView = findViewById(R.id.card_camera)
+        val btnScan = findViewById<View>(R.id.btn_scan)
+        // Set up an OnClickListener for the CardView
+        cardCamera.setOnClickListener {
+            // Start the UploadActivity when the CardView is clicked
+            startActivity(Intent(this@MainActivity, UploadActivity::class.java))
+        }
+        btnScan.setOnClickListener{
+            // Start the UploadActivity when btnScan is clicked
+            startActivity(Intent(this@MainActivity, UploadActivity::class.java))
+        }
+    }
+
+    private fun setBottomNavigationView(){
         setSupportActionBar(binding.bottomAppBar)
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
@@ -35,14 +55,6 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
-
     }
 
-
-//    fun onLogoutClick(view: View) {
-//        // Panggil metode logout() atau kode logout yang Anda inginkan di sini
-//        val profileFragment = supportFragmentManager.findFragmentById(R.id.profileFragment) as? ProfileFragment
-//        profileFragment?.logout()
-//    }
 }
